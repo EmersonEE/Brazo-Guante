bd4bad5d726822dd884458e2482e93f8bec56784
EmersonEE <perezemerson85@gmail.com>
2024-05-17 21:23:39 -0600

    Agregando Librerias, Pines Servo y Botones

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
+#include <OneButton.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// D11, D10, D9, D6 Para los Servos
+// D5, D4, D3, D2 Para los Botones
+// A0 para el potenciometro
+// A1 a A5 para leds
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Motor Alcanse 
+OneButton UpM1(5, true);
+OneButton DownM1(4, true);
+
+// Motor Altura
+OneButton UpM2(3, true);
+OneButton DownM2(2, true);
+
+Servo Base;
+Servo Alcanse;
+Servo Altura;
+Servo Pinza;
+
+
+    void setup()
+{
+  Base.attach(11);
+  Alcanse.attach(10);
+  Altura.attach(9);
+  Pinza.attach(6);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
 }
